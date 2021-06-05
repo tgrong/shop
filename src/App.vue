eac8ea044f7736116a03253f005a4fc38c52bc71
SKY-20200524GRD\Administrator <tao_guo_rong@163.com>
2021-06-05 15:32:07 +0800

    12

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <SayHello />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SayHello from "./components/ch3/SayHello.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    SayHello
   }
-}
+};
 </script>
 
 <style>
---
+++ src/components/ch3/SayHello.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div>{{ name }}</div>
+    <div>{{ sayHelloName }}</div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      name: "李四",
+    };
+  },
+  computed: {
+    sayHelloName() {
+      return this.name + "，你好！";
+    }
+  },
+};
+</script>
+<style scoped>
+div {
+  margin: 10px;
+  text-align: left;
+}
+</style>
