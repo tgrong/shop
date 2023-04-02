4120ff1adf35fe9cd3b0cdf5d51bc6583a9bf1cd
SKY-20200524GRD\Administrator <tao_guo_rong@163.com>
2023-04-02 18:15:05 +0800

    第8章

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,9 @@ const routes = [
         path: '/list',
         name: 'list',
         component: () => import('../views/list.vue'),
+        meta:{
+            isNoLogin:true
+        },
         children: [
             {
                 path: 'grade',
@@ -20,10 +23,76 @@ const routes = [
                 ]
             }
         ]
+    },
+    {
+        path: '/list/:gradeId',
+        name: 'list2',
+        component: () => import('../views/list.vue')
+    },
+    {
+        path: '/stulist',
+        name: 'stulist',
+        component: () => import('../views/ch8/stuList.vue')
+    },
+    {
+        path: '/login',
+        name: 'login',
+        component: () => import('../views/login.vue')
+    },
+    {
+        path: '/dispstu/:id',
+        name: 'dispstu',
+        component: () => import('../views/ch8/dispStu.vue')
+    },
+    {
+        path: '/error',
+        redirect: '/list',
+      },
+    // {
+    //     path: '/error',
+    //     redirect: {
+    //         name: 'list',
+    //         query: {
+    //             from: 'redirect',
+    //         }
+    //     }
+    // }
+    // {
+    //     path: '/error',
+    //     redirect: () => {
+    //         // 从当前登录用户信息中获取角色Id
+    //         const { roleId } = loginInfo
+
+    //         // 根据不同角色进行跳转
+    //         switch (roleId) {
+    //             // 管理员
+    //             case 1:
+    //                 return '/admin'
+
+    //             // 普通用户
+    //             case 2:
+    //                 return '/home'
+
+    //             // 其他
+    //             default:
+    //                 return '/login'
+    //         }
+    //     }
+    // }
+    {
+        path: '/:pathMatch(.*)*',
+        name: '404',
+        component: () => import('../views/404.vue'),
     }
 ]
 const router = createRouter({
     history: createWebHistory(process.env.BASE_URL),
     routes
 })
+router.beforeEach((to, from) => {
+    console.log(to,from)
+    const loginUser = localStorage.getItem("loginUser")
+    if (!loginUser) return '/login'
+  })
+  
 export default router
