77c106a13d5b3c2e9520070dbcb4ee908d80efe8
SKY-20200524GRD\Administrator <tao_guo_rong@163.com>
2023-05-07 21:17:23 +0800

    123

--- src/components/ch9/BaseRequest.vue
+++ src/components/ch9/BaseRequest.vue
@@ -8,6 +8,7 @@
     <div class="iframe">
         <div class="i-left">
             <button @click="onSendRequest">发送请求</button>
+            <button @click="onCacheRequest">缓存数据</button>
         </div>
     </div>
 </template>
@@ -20,10 +21,54 @@ export default {
     },
     methods: {
         onSendRequest() {
+            // 1. 设置一个key值为cacheData的localStorage对象
+            localStorage.setItem("cacheData", "tgrong");
+            // 2. 获取key值为cacheData的localStorage对象
+            localStorage.getItem("cacheData");
+            // 3. 删除key值为cacheData的localStorage对象
+            localStorage.removeItem("cacheData")
             this.data = "loading...";
             this.$http.get('/?day=1-1').then(d => {
                 this.data = d.data
+
+            }, err => {
+                console.log(err.message)
             })
+        },
+        onRequest() {
+            this.$http.get('/?day=1-1').then(d => {
+                this.data = d.data
+                // 请求成功后，再缓存返回数据
+                localStorage.setItem("cacheBase", d.data)
+                // 缓存数据保存时的时间
+                localStorage.setItem("cacheTime", (new Date().getTime()));
+            }, err => {
+                console.log(err.message)
+            })
+        },
+        onDiffTime(oTime, nTime, len) {
+            let _oM = parseInt(oTime / 1000 / 60);
+            let _nM = parseInt(nTime / 1000 / 60);
+            console.log(_nM, _oM, _nM - _oM)
+            if (_nM - _oM <= len) {
+                return true
+            } else {
+                return false;
+            }
+        },
+        onCacheRequest() {
+            this.data = "loading...";
+            // 先判断是否有缓存数据
+            if (localStorage.getItem("cacheBase") && localStorage.getItem("cacheTime")) {
+                // 判断时间否是规则内
+                if (this.onDiffTime(localStorage.getItem("cacheTime"), (new Date()).getTime(), 3)) {
+                    this.data = localStorage.getItem("cacheBase");
+                } else {
+                    this.onRequest();
+                }
+            } else {
+                this.onRequest();
+            }
         }
     }
 }
@@ -42,7 +87,10 @@ export default {
     display: flex;
     align-items: center;
 }
-
+.i-left button{
+    margin-right: 5px;
+}
+/*  */
 .iframe:last-child {
     border-top: none;
     background-color: #eee;
