ffefd985e37202fe456c1235a30ffd04f897a64d
SKY-20200524GRD\Administrator <tao_guo_rong@163.com>
2023-06-17 13:53:02 +0800

    init

--- src/main.js
+++ src/main.js
@@ -3,10 +3,14 @@ import App from './App.vue'
 import Global from './components/ch6/Global'
 import router from './router/index'
 import request from './plugins/axios';
-// import pinia from "./store/index"
+import pinia from "./store/index";
+import Vant from 'vant'
+import 'vant/lib/index.css';
 let app = createApp(App);
 app.config.globalProperties.$http = request;
 app.component("Global", Global);
 app.use(router)
-// app.use(pinia);
+app.use(Vant);
+app.use(pinia);
+
 app.mount('#app')
